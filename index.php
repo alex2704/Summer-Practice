c69299bf1f74ac9e9c5aae85d2b6087ad39822d9
alex2704 <34662549+alex2704@users.noreply.github.com>
2019-09-30 01:52:18 +0300

    доделал авторизацию полностью;

--- index.php
+++ index.php
@@ -1,16 +1,45 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
-	<meta charset="UTF-8" >
-	<meta name="viewport" content="width=device-width, initial scale=1">
+	<meta charset="UTF-8">
+	<title>Регистрация</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<title>Summer practice</title>
 </head>
 <body>
-	привет
+<?php
+require('connect.php');
+
+if(isset($_POST['username']) and isset($_POST['password'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$query = "INSERT INTO users (username, password) VALUES ('$username','$password')";
+	$result = mysqli_query($connection, $query);
+
+	if($result){
+		$smsg = "Регистрация прошла успешно";
+	} else{
+		$fsmsg = "Ошибка";
+	}
+}
+?>
+	<div class="log-box">
+		<h2>Регистрация</h2>
+		<?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"><?php echo $smsg; ?></div><?php } ?>
+		<?php if(isset($fsmsg)){ ?><div class="alert alert-danger" role="alert"><?php echo $fsmsg; ?></div><?php } ?>
+		<form method="POST">
+			<div class="log-inputBox">
+				<input type="text" name="username" required="">
+				<label for="">Логин</label>
+			</div>
+			<div class="log-inputBox">
+				<input type="password" name="password" required="">
+				<label for="">Пароль</label>
+			</div>
+			<input type="submit" name="" value="Зарегестрироваться">
+			<a href="login.php" class="btn btn-a-login">Уже есть аккаунт?</a>
+		</form>
+	</div>
 </body>
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </html>
--- login.php
+++ login.php
@@ -2,31 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Авторизация</title>
+	<title>Регистрация</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
-<?php
-require('connect.php');
-
-if(isset($_POST['username']) && isset($_POST['password'])){
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-
-	$query = "INSERT INTO users (username, password) VALUES ('$username','$password')";
-	$result = mysqli_query($connection, $query);
-
-	if($result){
-		$smsg = "Регистрация прошла успешно";
-	} else{
-		$fsmsg = "Ошибка";
-	}
-}
-?>
 	<div class="log-box">
-		<h2>Авторизоация</h2>
-		<?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"><?php echo $smsg; ?></div><?php } ?>
-		<?php if(isset($fsmsg)){ ?><div class="alert alert-danger" role="alert"><?php echo $fsmsg; ?></div><?php } ?>
+		<h2>Вход</h2>
 		<form method="POST">
 			<div class="log-inputBox">
 				<input type="text" name="username" required="">
@@ -37,7 +19,35 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 				<label for="">Пароль</label>
 			</div>
 			<input type="submit" name="" value="Войти">
+			<a href="index.php" class="btn btn-a-login">Регистрация</a>
 		</form>
 	</div>
+
+<?php
+session_start();
+require('connect.php');
+
+if(isset($_POST['username']) and isset($_POST['password'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$query = "SELECT * FROM users  WHERE username='$username' and password='$password'";
+	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+	$count = mysqli_num_rows($result);
+
+	if ($count == 1){
+		$_SESSION['username'] = $username;
+	}else {
+		$fmsg = "Ошибка";
+	}
+}
+
+if (isset($_SESSION['username'])){
+	$username = $_SESSION['username'];
+	echo "Привет" . $username . "";
+	echo "Вы вошли";
+	echo "<a href='logout.php' class='btn btn-lg btn-primary' > Logout </a>";
+}
+?>
 </body>
 </html>
